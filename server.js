430e478928bda12d800d445b9c6ad58f8341a999
Ariff M <munshicorp@gmail.com>
2023-10-27 22:30:35 +0800

    Add database connection file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+require('dotenv').config();
+const dbConnect = require("./dbConnect");
+dbConnect.connectMySQL();
+
 const app = express();
 const port = process.env.PORT || 8080;
+
 
 app.listen(port,()=>{
   console.log("Listening on port ", port);
